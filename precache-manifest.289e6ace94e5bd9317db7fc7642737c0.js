d50dc92b6176ef3e2099fd7d4cd7083b6b6a99cb
jimmyhwang4 <35846105+jimmyhwang4@users.noreply.github.com>
2020-06-17 17:42:10 -0700

    Updates

---
+++ precache-manifest.289e6ace94e5bd9317db7fc7642737c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4b8d15d400e34038d8cdece3749777",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "f2bd88d519b767bb3893",
+    "url": "/covid-19/static/css/main.882fc843.chunk.css"
+  },
+  {
+    "revision": "8ae9b3a1e5e0d2f656f0",
+    "url": "/covid-19/static/js/2.87ad738e.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/covid-19/static/js/2.87ad738e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2bd88d519b767bb3893",
+    "url": "/covid-19/static/js/main.8601f9f0.chunk.js"
+  },
+  {
+    "revision": "7d8481129e409e180469",
+    "url": "/covid-19/static/js/runtime-main.8ab1154a.js"
+  }
+]);
